6f10ee2061190c388d71ded089b3c34e586baa04
Phạm Trí Nguyên <louis0995@gmail.com>
2016-10-03 13:28:54 +0700

    Edit Button
    Sua lai nut back

--- src/lab6/com/lab6_ketqua.java
+++ src/lab6/com/lab6_ketqua.java
@@ -1,12 +1,17 @@
 package lab6.com;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 import manhinhdieukhien.com.R;
 
-public class lab6_ketqua extends Activity {
+public class lab6_ketqua extends Activity implements OnClickListener {
 	TextView txtRelease;
+	Button btnBack;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -14,9 +19,21 @@ public class lab6_ketqua extends Activity {
 		setContentView(R.layout.activity_lab6_ketqua);
 		
 		txtRelease = (TextView) findViewById(R.id.txtRelease);
+		btnBack = (Button) findViewById(R.id.btnBack);
+		
+		btnBack.setOnClickListener(this);
+		
 		
 		String gtnhan = getIntent().getExtras().getString("hoten");
 		txtRelease.setText(gtnhan);				
+	}
+	@Override
+	public void onClick(View v) {
+		if(v==btnBack){
+			Intent intent = new Intent(this,lab6.class);
+			startActivity(intent);
+		}
+		
 	}
 
 }
